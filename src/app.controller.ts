bde164f93e4cea2a411617f6ee0490c7293faab2
kimuragen <61538808+kimuragen23@users.noreply.github.com>
2023-12-14 23:41:05 +0900

    app 관리자 라우트 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,9 +2,16 @@ import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
-  @Get()
-  @Render('index2')
+
+  @Get('')
+  @Render('index')
   root() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('dpstPrcsn')
+  @Render('index2')
+  dpstPrcsn() {
     return { message: 'Hello world!' };
   }
 }
