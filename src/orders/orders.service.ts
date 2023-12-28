85cb409c9e1da09a29ef5e8849350f7ab4ddef14
kimuragen <61538808+kimuragen23@users.noreply.github.com>
2023-12-28 18:02:19 +0900

    시간에 오후, 오전 삭제

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -232,7 +232,7 @@ export class OrdersService {
     const hour = date.getHours();
     const minute = date.getMinutes();
     const amPm = hour >= 12 ? '오후' : '오전';
-    const order_date = `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분 ${amPm}`;
+    const order_date = `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분`;
     return order_date
   }
 }
